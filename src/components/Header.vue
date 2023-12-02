41cacf6d05130b7b165fd2948bd6e00f72cd5676
HuyDong2000 <65489886+HuyDong2000@users.noreply.github.com>
2023-12-02 22:06:59 +0700

    update header

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Add Restaurant</a>
+    <a href="#">Update Restaurant</a>
+    <a href="#">LogOut</a>
+</div>
+</template>
+
+<script>
+export default {
+    name:'PageHeader',
+}
+</script>
+
+<style>
+body{
+    margin: 0px;
+    padding: 0px;
+}
+.nav{
+    background-color: #333;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+.nav a{
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 16px 14px;
+    text-decoration: none;
+    font-size: 16px;
+    margin-right: 4px;
+
+}
+.nav a:hover{
+    background-color: #fff;
+    color: #222;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,13 @@
 <template>
+  <Header/>
   <h3>Home</h3>
 </template>
 <script>
+import Header from './Header.vue'
 export default {
     name: 'PageHome',
+    components:{
+      Header
+    },
 }
 </script>
